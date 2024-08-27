abfab75cd5aee821aa1c476123bed1c361c19e2a
leaco <leaco.li@learnable.ai>
2024-08-27 19:06:34 +0800

    init

--- transform2.py
+++ transform2.py
@@ -3,10 +3,12 @@ import torch.nn as nn
 import torch.optim as optim
 
 # Prepare the data
-x_train = torch.tensor([[[1.6, 1.6], [2, 1], [6.3, 1.1]],
-                         [[2, 1], [6.3, 1.1], [1.2, 1.3]],
-                         [[6.3, 1.1], [1.2, 1.3], [5.1, 1.9]]], dtype=torch.float32)
-y_train = torch.tensor([1.2, 5.1, 7.9], dtype=torch.float32).view(-1, 1)
+x_train = torch.tensor([
+    [[1, 1], [2, 2], [3, 3]],
+    [[2, 2], [3, 3], [4, 4]],
+    [[3, 3], [4, 4], [5, 5]],
+], dtype=torch.float32)
+y_train = torch.tensor([4, 5, 6], dtype=torch.float32).view(-1, 1)
 
 # Define the Transformer model
 class TransformerModel(nn.Module):
